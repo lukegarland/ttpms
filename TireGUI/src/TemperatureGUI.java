287cbcb5c5f069ca61904a163b00dc8d4a743602
lukegarland <luke.garland01@gmail.com>
2020-03-07 12:15:17 -0700

    Make main more modular

--- TireGUI/src/TemperatureGUI.java
+++ TireGUI/src/TemperatureGUI.java
@@ -1,35 +1,82 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import com.fazecast.jSerialComm.*;
 
 public class TemperatureGUI {
-	public static void main(String[] args) {
-       
-        // determine which serial port to use
-        SerialPort ports[] = SerialPort.getCommPorts();
-        System.out.println("Select a port:");
-        int i = 1;
-        for(SerialPort port : ports) {
-                System.out.println(i++ + ". " + port.getSystemPortName());
-        }
-        Scanner s = new Scanner(System.in);
-        int chosenPort = s.nextInt();
-        s.close();
-        // open and configure the port
-        SerialPort port = ports[chosenPort - 1];
-        if(port.openPort()) {
-                System.out.println("Successfully opened the port.");
-        } else {
-                System.out.println("Unable to open the port.");
-                return;
-        }
-        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-        
+	
+	
+	
+	public static void main(String[] args) 
+	{
+              
+        SerialPort port = getPortFromSystemIn();
         // enter into an infinite loop that reads from the port and updates the GUI
         Scanner data = new Scanner(port.getInputStream());
-        while(data.hasNextLine()) {
+        
+        
+        while(data.hasNextLine()) 
+        {
                 System.out.println(data.nextLine());
         }
+        
         data.close();
+	}
+	
+	private static SerialPort getPortFromSystemIn()
+	{
+
+	     System.out.println("Select a port:");
+
+	     printPorts();
+	     
+	     Scanner s = new Scanner(System.in);
+	     
+	     int chosenPort = 0;
+	     
+	     while(true)
+	     {    
+	    	try 
+	     	{
+	    		 chosenPort = s.nextInt();
+	    		 break;
+	     	}
+	     	catch(InputMismatchException e)
+	     	{
+	     		System.err.println("Error: Invalid input, try again");
+	     	}
+	     }
+	     
+	     s.close();
+	     
+	     // open and configure the port
+	     SerialPort port = SerialPort.getCommPorts()[chosenPort - 1];
+	     
+	     
+	     if(port.openPort())
+	     {
+	    	 System.err.println("Successfully opened the port.");
+	     } 
+	     else 
+	     {
+	    	 System.err.println("Unable to open the port.");
+	     }
+
+	     port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+	     
+	     return port;
+	        
+	}
+	
+	private static void printPorts()
+	{
+		 SerialPort ports[] = SerialPort.getCommPorts();
+		 
+	     int i = 1;
+	     for(SerialPort port : ports) 
+	     {
+	                System.out.println(i++ + ":\t" + port.getDescriptivePortName());
+	     }
+	     
 	}
 }
 
