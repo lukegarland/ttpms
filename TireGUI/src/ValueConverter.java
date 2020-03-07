a7be7fee1f142589eb746ef58322e887738e46b0
lukegarland <luke.garland01@gmail.com>
2020-03-07 15:35:46 -0700

    Add ADC to physical value converter

---
+++ TireGUI/src/ValueConverter.java
@@ -0,0 +1,32 @@
+
+/**
+ * @author lukeg
+ * @since March 3, 2020
+ */
+public class ValueConverter {
+
+	
+	public static double convertTokPa(int ADCValue)
+	{
+		
+		
+		return 0.0;
+	}
+	
+	
+	
+	public static double convertAmbientTemperature(int ADCValue)
+	{
+		
+		
+		return 0.0;
+	}
+	
+	public static double convertIRTemperature(int ambientADCValue, int IRADCValue)
+	{
+		
+		
+		return 0.0;
+	}
+	
+}
